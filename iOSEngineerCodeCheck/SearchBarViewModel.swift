5aadd21d67f6939aec9254caf85bd9e852d91e03
Kanta Oikawa <k.oikawa@kantacky.com>
2023-07-09 17:44:43 +0900

    Add SearchBarViewModel

---
+++ iOSEngineerCodeCheck/SearchBarViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  SearchBarViewModel.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by Kanta Oikawa on 2023/07/09.
+//  Copyright © 2023 YUMEMI Inc. All rights reserved.
+//
+
+import Foundation
