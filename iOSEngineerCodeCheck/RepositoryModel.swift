4153c3b71a7ebe507af74bb174920dacdb431d35
Kanta Oikawa <k.oikawa@kantacky.com>
2023-07-09 12:25:57 +0900

    Add RepositoryModel

---
+++ iOSEngineerCodeCheck/RepositoryModel.swift
@@ -0,0 +1,9 @@
+//
+//  RepositoryModel.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by Kanta Oikawa on 2023/07/09.
+//  Copyright © 2023 YUMEMI Inc. All rights reserved.
+//
+
+import Foundation
